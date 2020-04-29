d3291cedb40afdab8f5e1d3a02ecfc9d0aa30b39
Michael <michael.nik999@gmail.com>
2020-04-29 17:15:40 +0300

    add textFlow to fragments

--- src/main/kotlin/com/kek/chat/gui/fragment/ChatFragment.kt
+++ src/main/kotlin/com/kek/chat/gui/fragment/ChatFragment.kt
@@ -1,20 +1,31 @@
 package com.kek.chat.gui.fragment
 
+import com.kek.chat.gui.MainController
 import javafx.beans.property.SimpleStringProperty
 import javafx.scene.Parent
 import javafx.scene.text.TextFlow
 import tornadofx.*
 
 class ChatFragment() : Fragment("") {
-    val name = SimpleStringProperty("")
+    private val name = SimpleStringProperty("")
+    val controller: MainController by inject()
 
+    private val textFlow = textflow { text("init\n") }
     override val root = vbox {
-        label("aaaaaaaaaaaa")
+        add(textFlow)
         prefWidth = 568.0
         prefHeight = 320.0
     }
     override fun onDock() {
         name.value = params["name"] as String?
         title = name.value ?: ""
+
+        addText("heh")
+        addText("kek")
     }
+
+    private fun addText(newText: String) {
+        textFlow.add(text(newText + "\n"))
+    }
+
 }
